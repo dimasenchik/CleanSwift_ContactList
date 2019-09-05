4df8861fa4db04c6a98e47d53660585343caf650
Senchik Dima <dmitriy.senchik@gmail.com>
2019-09-05 20:16:03 +0300

    Initial Commit

---
+++ ContactsApp_CleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp_CleanSwift
+//
+//  Created by Dima Senchik on 9/5/19.
+//  Copyright © 2019 Dima Senchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
